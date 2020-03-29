a8cae4a030e2df5d370020d23baf879cf8efa08a
owenwattimena <wentoxwtt@gmail.com>
2020-03-30 02:47:01 +0900

    ramove catch

--- assets/js/cloud-firestore.js
+++ assets/js/cloud-firestore.js
@@ -52,7 +52,5 @@ function data_maluku() {
             // doc.data() will be undefined in this case
             console.log("No such document!");
         }
-    }).catch(function (error) {
-        console.log("Error getting document:" + error);
     });
 }
