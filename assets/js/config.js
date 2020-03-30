7b30e54d85b155b82d5c581ca71a7bda64fd4329
owenwattimena <wentoxwtt@gmail.com>
2020-03-30 16:41:49 +0900

    add link in footer

--- assets/js/config.js
+++ assets/js/config.js
@@ -2,5 +2,5 @@ const BASE_URL = 'https://owenwattimena.github.io/covid19-stats/';
 
 $('.navbar-brand').attr('href', BASE_URL);
 $('footer').html(`
-<p class="text-center text-light p-2">#wentoxwtt</p>
+<p class="text-center text-light p-2"><a href='https://owenwattimena.github.io/'>#wentoxwtt</a></p>
 `);
